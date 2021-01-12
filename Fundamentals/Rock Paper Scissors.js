a4c09ea520729163332c908b57f74bdd75796424
Sunmon <blackmilk274@gmail.com>
2021-01-12 22:50:48 +0900

    add: Fundamental 2문제

---
+++ Fundamentals/Rock Paper Scissors.js
@@ -0,0 +1,24 @@
+/**
+ * @Kata Rock Paper Scissors
+ * @link https://www.codewars.com/kata/5672a98bdbdd995fad00000f/javascript
+ * @Kyu 8
+ * @date 2021-01-12
+ * @author sunmon
+ */
+
+// Sum Numbers
+const rps = (p1, p2) => {
+  const m = new Map([
+    ['scissors', 0],
+    ['rock', 1],
+    ['paper', 2],
+  ]);
+
+  const results = [
+    ['Draw', 'Player 2 won', 'Player 1 won'],
+    ['Player 1 won', 'Draw', 'Player 2 won'],
+    ['Player 2 won', 'Player 1 won', 'Draw'],
+  ];
+
+  return `${results[m.get(p1)][m.get(p2)]}!`;
+};
