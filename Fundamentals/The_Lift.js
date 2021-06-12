0ed7f7543ab5a083a2683b975829bb41a287b358
sunmon <blackmilk274@gmail.com>
2021-06-12 22:48:21 +0900

    [add] The Lift

---
+++ Fundamentals/The_Lift.js
@@ -0,0 +1,17 @@
+/**
+ * @Kata The Lift
+ * @link https://www.codewars.com/kata/58905bfa1decb981da00009e/
+ * @Kyu 3
+ * @Training Fundamentals
+ * @date 2021-06-12
+ * @author sunmon
+ */
+
+ var theLift = function(queues, capacity) {
+  // Your code here!
+  return [999]
+}
+
+
+
+
